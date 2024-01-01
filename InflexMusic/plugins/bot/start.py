7e596be01a73830511f5274ce526cce012b7ff05
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-01-01 12:16:53 +0530

    Update start.py

--- InflexMusic/plugins/bot/start.py
+++ InflexMusic/plugins/bot/start.py
@@ -87,6 +87,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
+        await message.reply_sticker(
         await message.reply_photo(
             photo=random.choice(START_IMG_URL),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
