d505a8da48e4753c51154fafad6311ab9507317f
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-01-01 12:24:41 +0530

    Update start.py

--- InflexMusic/plugins/bot/start.py
+++ InflexMusic/plugins/bot/start.py
@@ -34,7 +34,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker(
+            await message.reply_sticker("CAACAgUAAxkBAAELExhlkmFbAarTvcZZuxVhtMo0SJcWIwACQwoAAvPiqFR5n0OrwQHpUjQE")
             return await message.reply_photo(
                 photo=random.choice(START_IMG_URL),
                 caption=_["help_1"].format(config.SUPPORT_GROUP),
@@ -87,7 +87,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker(
+        await message.reply_sticker("CAACAgUAAxkBAAELExhlkmFbAarTvcZZuxVhtMo0SJcWIwACQwoAAvPiqFR5n0OrwQHpUjQE")
         await message.reply_photo(
             photo=random.choice(START_IMG_URL),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
